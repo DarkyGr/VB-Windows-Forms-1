0074340cf023a8d72b333e06d5f54f2f21f6162a
Guillermo Riaño <guilledark17@gmail.com>
2023-06-11 14:22:52 -0600

    ✅ Move the Form from the Panel

--- VB_WindowsForms_CRUD_1/FormReader.Designer.vb
+++ VB_WindowsForms_CRUD_1/FormReader.Designer.vb
@@ -22,18 +22,60 @@ Partial Class FormReader
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.PnlTitle = New System.Windows.Forms.Panel()
+        Me.LblTitle = New System.Windows.Forms.Label()
+        Me.BtnClose = New System.Windows.Forms.Button()
+        Me.PnlTitle.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'PnlTitle
+        '
+        Me.PnlTitle.BackColor = System.Drawing.Color.DarkOrange
+        Me.PnlTitle.Controls.Add(Me.BtnClose)
+        Me.PnlTitle.Controls.Add(Me.LblTitle)
+        Me.PnlTitle.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PnlTitle.Location = New System.Drawing.Point(0, 0)
+        Me.PnlTitle.Name = "PnlTitle"
+        Me.PnlTitle.Size = New System.Drawing.Size(983, 71)
+        Me.PnlTitle.TabIndex = 0
+        '
+        'LblTitle
+        '
+        Me.LblTitle.AutoSize = True
+        Me.LblTitle.Font = New System.Drawing.Font("Cambria", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblTitle.Location = New System.Drawing.Point(39, 22)
+        Me.LblTitle.Name = "LblTitle"
+        Me.LblTitle.Size = New System.Drawing.Size(82, 22)
+        Me.LblTitle.TabIndex = 0
+        Me.LblTitle.Text = "Readers"
+        '
+        'BtnClose
+        '
+        Me.BtnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnClose.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnClose.Location = New System.Drawing.Point(926, 12)
+        Me.BtnClose.Name = "BtnClose"
+        Me.BtnClose.Size = New System.Drawing.Size(45, 45)
+        Me.BtnClose.TabIndex = 1
+        Me.BtnClose.Text = "X"
+        Me.BtnClose.UseVisualStyleBackColor = True
         '
         'FormReader
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(983, 674)
+        Me.Controls.Add(Me.PnlTitle)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "FormReader"
         Me.Text = "Form1"
+        Me.PnlTitle.ResumeLayout(False)
+        Me.PnlTitle.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents PnlTitle As Panel
+    Friend WithEvents LblTitle As Label
+    Friend WithEvents BtnClose As Button
 End Class
--- VB_WindowsForms_CRUD_1/FormReader.vb
+++ VB_WindowsForms_CRUD_1/FormReader.vb
@@ -1,5 +1,29 @@
 ﻿Public Class FormReader
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles BtnClose.Click
+
+    End Sub
+
+    Dim ex, ey As Integer
+    Dim drag As Boolean
+
+    'Define the variables when click the mouse
+    Private Sub PnlTitle_MouseDown(sender As Object, e As MouseEventArgs) Handles PnlTitle.MouseDown
+        ex = e.X
+        ey = e.Y
+        Drag = True
+    End Sub
+
+    'Calculate the position for the panel
+    Private Sub PnlTitle_MouseMove(sender As Object, e As MouseEventArgs) Handles PnlTitle.MouseMove
+        If drag Then
+            Me.Location = Me.PointToScreen(New Point(FormReader.MousePosition.X - Me.Location.X - ex,
+                                                     FormReader.MousePosition.Y - Me.Location.Y - ey))
+        End If
+    End Sub
+
+    'When the drag is false the panel is stop
+    Private Sub PnlTitle_MouseUp(sender As Object, e As MouseEventArgs) Handles PnlTitle.MouseUp
+        drag = False
     End Sub
 End Class
