7dfed24804e10054adfd2d87d72f15046def8bf6
Guillermo Riaño <guilledark17@gmail.com>
2023-06-13 12:25:42 -0600

    ✅ Adding Create Stored Procedure

--- DB/db_VBWindowsFormsCrud_1.sql
+++ DB/db_VBWindowsFormsCrud_1.sql
@@ -3,6 +3,7 @@ create database db_VBWindowsFormsCrud_1
 
 use db_VBWindowsFormsCrud_1
 
+
 -- ============ TABLES ===============
 create table Reader(
 	readerId varchar(13) primary key,
@@ -11,3 +12,25 @@ create table Reader(
 	readerAddress varchar(100),
 	readerObservations varchar(100)
 )
+
+
+-- ============ Stored Procedures ===============
+-- *** CREATE ***
+create proc sp_CreateReader(
+	@readerId varchar(13),
+	@readerName varchar(50),
+	@readerPhone int,
+	@readerAddress varchar(100),
+	@readerObservations varchar(100)
+)
+as
+if EXISTS (Select readerId from Reader where readerId = @readerId)
+	RAISERROR('The reader already exists', 16, 1)
+else	
+	insert into Reader values (
+		@readerId,
+		@readerName,
+		@readerPhone,
+		@readerAddress,
+		@readerObservations
+	)
