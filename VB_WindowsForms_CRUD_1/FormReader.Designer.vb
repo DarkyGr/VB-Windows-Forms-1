a632e300d12304a52d1103e67c86908c6abee631
Guillermo Riaño <guilledark17@gmail.com>
2023-06-12 22:37:23 -0600

    ✔️ Adding form to Add Reader

--- VB_WindowsForms_CRUD_1/FormReader.Designer.vb
+++ VB_WindowsForms_CRUD_1/FormReader.Designer.vb
@@ -27,15 +27,29 @@ Partial Class FormReader
         Me.BtnClose = New System.Windows.Forms.Button()
         Me.LblTitle = New System.Windows.Forms.Label()
         Me.PnlSearch = New System.Windows.Forms.Panel()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.TxtSearch = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.PnlContent = New System.Windows.Forms.Panel()
+        Me.PnlData = New System.Windows.Forms.Panel()
+        Me.TxtRObservations = New System.Windows.Forms.TextBox()
+        Me.LblRObservations = New System.Windows.Forms.Label()
+        Me.TxtRAddress = New System.Windows.Forms.TextBox()
+        Me.LblRAddress = New System.Windows.Forms.Label()
+        Me.TxtRPhone = New System.Windows.Forms.TextBox()
+        Me.LblRPhone = New System.Windows.Forms.Label()
+        Me.TxtRName = New System.Windows.Forms.TextBox()
+        Me.LblRName = New System.Windows.Forms.Label()
+        Me.TxtRId = New System.Windows.Forms.TextBox()
+        Me.lblRId = New System.Windows.Forms.Label()
+        Me.DGReaders = New System.Windows.Forms.DataGridView()
         Me.PnlSide = New System.Windows.Forms.Panel()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.PnlTitle.SuspendLayout()
         Me.PnlSearch.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PnlContent.SuspendLayout()
+        Me.PnlData.SuspendLayout()
+        CType(Me.DGReaders, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PnlSide.SuspendLayout()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -86,6 +100,14 @@ Partial Class FormReader
         Me.PnlSearch.Size = New System.Drawing.Size(983, 66)
         Me.PnlSearch.TabIndex = 1
         '
+        'TxtSearch
+        '
+        Me.TxtSearch.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtSearch.Location = New System.Drawing.Point(101, 19)
+        Me.TxtSearch.Name = "TxtSearch"
+        Me.TxtSearch.Size = New System.Drawing.Size(374, 26)
+        Me.TxtSearch.TabIndex = 3
+        '
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
@@ -96,23 +118,141 @@ Partial Class FormReader
         Me.PictureBox1.TabIndex = 2
         Me.PictureBox1.TabStop = False
         '
-        'TxtSearch
-        '
-        Me.TxtSearch.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TxtSearch.Location = New System.Drawing.Point(101, 19)
-        Me.TxtSearch.Name = "TxtSearch"
-        Me.TxtSearch.Size = New System.Drawing.Size(374, 26)
-        Me.TxtSearch.TabIndex = 3
-        '
         'PnlContent
         '
         Me.PnlContent.BackColor = System.Drawing.Color.White
+        Me.PnlContent.Controls.Add(Me.PnlData)
+        Me.PnlContent.Controls.Add(Me.DGReaders)
         Me.PnlContent.Controls.Add(Me.PnlSide)
         Me.PnlContent.Dock = System.Windows.Forms.DockStyle.Fill
         Me.PnlContent.Location = New System.Drawing.Point(0, 137)
         Me.PnlContent.Name = "PnlContent"
         Me.PnlContent.Size = New System.Drawing.Size(983, 537)
         Me.PnlContent.TabIndex = 2
+        '
+        'PnlData
+        '
+        Me.PnlData.Controls.Add(Me.TxtRObservations)
+        Me.PnlData.Controls.Add(Me.LblRObservations)
+        Me.PnlData.Controls.Add(Me.TxtRAddress)
+        Me.PnlData.Controls.Add(Me.LblRAddress)
+        Me.PnlData.Controls.Add(Me.TxtRPhone)
+        Me.PnlData.Controls.Add(Me.LblRPhone)
+        Me.PnlData.Controls.Add(Me.TxtRName)
+        Me.PnlData.Controls.Add(Me.LblRName)
+        Me.PnlData.Controls.Add(Me.TxtRId)
+        Me.PnlData.Controls.Add(Me.lblRId)
+        Me.PnlData.Location = New System.Drawing.Point(0, 0)
+        Me.PnlData.Name = "PnlData"
+        Me.PnlData.Size = New System.Drawing.Size(983, 537)
+        Me.PnlData.TabIndex = 2
+        '
+        'TxtRObservations
+        '
+        Me.TxtRObservations.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtRObservations.Location = New System.Drawing.Point(245, 313)
+        Me.TxtRObservations.Name = "TxtRObservations"
+        Me.TxtRObservations.Size = New System.Drawing.Size(606, 26)
+        Me.TxtRObservations.TabIndex = 9
+        '
+        'LblRObservations
+        '
+        Me.LblRObservations.AutoSize = True
+        Me.LblRObservations.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblRObservations.Location = New System.Drawing.Point(79, 316)
+        Me.LblRObservations.Name = "LblRObservations"
+        Me.LblRObservations.Size = New System.Drawing.Size(100, 19)
+        Me.LblRObservations.TabIndex = 8
+        Me.LblRObservations.Text = "Observations"
+        '
+        'TxtRAddress
+        '
+        Me.TxtRAddress.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtRAddress.Location = New System.Drawing.Point(245, 247)
+        Me.TxtRAddress.Name = "TxtRAddress"
+        Me.TxtRAddress.Size = New System.Drawing.Size(606, 26)
+        Me.TxtRAddress.TabIndex = 7
+        '
+        'LblRAddress
+        '
+        Me.LblRAddress.AutoSize = True
+        Me.LblRAddress.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblRAddress.Location = New System.Drawing.Point(79, 250)
+        Me.LblRAddress.Name = "LblRAddress"
+        Me.LblRAddress.Size = New System.Drawing.Size(120, 19)
+        Me.LblRAddress.TabIndex = 6
+        Me.LblRAddress.Text = "Reader Address"
+        '
+        'TxtRPhone
+        '
+        Me.TxtRPhone.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtRPhone.Location = New System.Drawing.Point(245, 177)
+        Me.TxtRPhone.Name = "TxtRPhone"
+        Me.TxtRPhone.Size = New System.Drawing.Size(606, 26)
+        Me.TxtRPhone.TabIndex = 5
+        '
+        'LblRPhone
+        '
+        Me.LblRPhone.AutoSize = True
+        Me.LblRPhone.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblRPhone.Location = New System.Drawing.Point(79, 180)
+        Me.LblRPhone.Name = "LblRPhone"
+        Me.LblRPhone.Size = New System.Drawing.Size(107, 19)
+        Me.LblRPhone.TabIndex = 4
+        Me.LblRPhone.Text = "Reader Phone"
+        '
+        'TxtRName
+        '
+        Me.TxtRName.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtRName.Location = New System.Drawing.Point(245, 115)
+        Me.TxtRName.Name = "TxtRName"
+        Me.TxtRName.Size = New System.Drawing.Size(606, 26)
+        Me.TxtRName.TabIndex = 3
+        '
+        'LblRName
+        '
+        Me.LblRName.AutoSize = True
+        Me.LblRName.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblRName.Location = New System.Drawing.Point(79, 118)
+        Me.LblRName.Name = "LblRName"
+        Me.LblRName.Size = New System.Drawing.Size(103, 19)
+        Me.LblRName.TabIndex = 2
+        Me.LblRName.Text = "Reader Name"
+        '
+        'TxtRId
+        '
+        Me.TxtRId.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtRId.Location = New System.Drawing.Point(245, 50)
+        Me.TxtRId.Name = "TxtRId"
+        Me.TxtRId.Size = New System.Drawing.Size(606, 26)
+        Me.TxtRId.TabIndex = 1
+        '
+        'lblRId
+        '
+        Me.lblRId.AutoSize = True
+        Me.lblRId.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRId.Location = New System.Drawing.Point(79, 53)
+        Me.lblRId.Name = "lblRId"
+        Me.lblRId.Size = New System.Drawing.Size(79, 19)
+        Me.lblRId.TabIndex = 0
+        Me.lblRId.Text = "Reader ID"
+        '
+        'DGReaders
+        '
+        Me.DGReaders.AllowUserToAddRows = False
+        Me.DGReaders.AllowUserToDeleteRows = False
+        Me.DGReaders.AllowUserToResizeColumns = False
+        Me.DGReaders.BackgroundColor = System.Drawing.Color.White
+        Me.DGReaders.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None
+        Me.DGReaders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGReaders.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DGReaders.EnableHeadersVisualStyles = False
+        Me.DGReaders.Location = New System.Drawing.Point(0, 0)
+        Me.DGReaders.Name = "DGReaders"
+        Me.DGReaders.RowHeadersVisible = False
+        Me.DGReaders.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DGReaders.Size = New System.Drawing.Size(851, 537)
+        Me.DGReaders.TabIndex = 1
         '
         'PnlSide
         '
@@ -126,7 +266,7 @@ Partial Class FormReader
         'PictureBox2
         '
         Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
-        Me.PictureBox2.Location = New System.Drawing.Point(20, 294)
+        Me.PictureBox2.Location = New System.Drawing.Point(20, 269)
         Me.PictureBox2.Name = "PictureBox2"
         Me.PictureBox2.Size = New System.Drawing.Size(100, 100)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
@@ -151,6 +291,9 @@ Partial Class FormReader
         Me.PnlSearch.PerformLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PnlContent.ResumeLayout(False)
+        Me.PnlData.ResumeLayout(False)
+        Me.PnlData.PerformLayout()
+        CType(Me.DGReaders, System.ComponentModel.ISupportInitialize).EndInit()
         Me.PnlSide.ResumeLayout(False)
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
@@ -166,4 +309,16 @@ Partial Class FormReader
     Friend WithEvents PnlContent As Panel
     Friend WithEvents PnlSide As Panel
     Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents DGReaders As DataGridView
+    Friend WithEvents PnlData As Panel
+    Friend WithEvents lblRId As Label
+    Friend WithEvents TxtRId As TextBox
+    Friend WithEvents TxtRObservations As TextBox
+    Friend WithEvents LblRObservations As Label
+    Friend WithEvents TxtRAddress As TextBox
+    Friend WithEvents LblRAddress As Label
+    Friend WithEvents TxtRPhone As TextBox
+    Friend WithEvents LblRPhone As Label
+    Friend WithEvents TxtRName As TextBox
+    Friend WithEvents LblRName As Label
 End Class
