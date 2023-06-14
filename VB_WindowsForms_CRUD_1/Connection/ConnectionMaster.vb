f4848764ebded64f96eec311b4234b8da5feb51f
Guillermo Riaño <guilledark17@gmail.com>
2023-06-14 11:58:30 -0600

    ✅ Adding Connection with SQLServer

---
+++ VB_WindowsForms_CRUD_1/Connection/ConnectionMaster.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+
+Module ConnectionMaster
+
+    Public connection As New SqlConnection("Server= localhost;Database= db_VBWindowsFormsCrud_1;User Id= sa; Password= 12345")
+
+    'Open connection
+    Sub OpenConnection()
+        If connection.State = 0 Then
+            connection.Open()
+        End If
+    End Sub
+
+    'Close connection
+    Sub CloseConnection()
+        If connection.State = 1 Then
+            connection.Close()
+        End If
+    End Sub
+
+End Module
